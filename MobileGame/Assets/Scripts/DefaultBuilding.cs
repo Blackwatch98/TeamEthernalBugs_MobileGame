8e9ec36134bff7c591c739bf0d94cf752a6b5f5d
Blackwatch98 <danielduczyminski@gmail.com>
2020-12-01 02:14:44 +0100

    sidebar extended, select building script and default building class impeleneted

---
+++ MobileGame/Assets/Scripts/DefaultBuilding.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultBuilding : MonoBehaviour
+{
+    private string name;
+    private bool isOwned = false;
+    void Start()
+    {
+        name = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MobileGame/Assets/Scripts/SelectBuildingScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectBuildingScript : MonoBehaviour
+{
+    private float holdDownStartTime;
+    private readonly float holdTimeLimit = 1f; // 1s
+    private GameObject selectedObject;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            holdDownStartTime = Time.time;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            float holdDownTime = Time.time - holdDownStartTime;
+            if (holdDownTime >= holdTimeLimit)
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, 100f))
+                {
+                    if (hit.transform && hit.transform.gameObject.name != "Plane")
+                    {
+                        print(hit.transform.gameObject.name);
+
+                    }
+                }
+                Debug.Log(holdDownTime + " tyle trzymałeś");
+            }
+        }
+    }
+}
