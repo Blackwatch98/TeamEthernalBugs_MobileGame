b2c348e29248a7d38e8d5cd117077c37a3330a05
Łukasz W <wojcikowski1@gmail.com>
2020-12-08 18:04:03 +0100

    Script now allows to pick profit very basically, and highlights building to yellow for 3 sec, label inserted to scene and camera perspective changed a little

--- MobileGame/Assets/Highlight.cs
+++ MobileGame/Assets/Highlight.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using UnityEngine.UI;
+using System;
 public class Highlight : MonoBehaviour
 {
-    double startTime;
-    double endTime;
+    int delayToPick = 5;
+    int startProfitforAll = 0;
+    int profitIncrease = 2;
+    double startTime, endTime, countToYellow, shiningStartTime, shiningEndTime;
+    string clickedBuilding = "";
+    Camera cam;
     Transform clickedObject;
     object[] obj;
-    List<List<Color>> originalColors;
+    List<Color> originalColorsOfSingleBuilding = new List<Color>();
+    List<List<Color>> originalColorsOfAllBuildings;
+    Text profitInfo;
+    Text tobaccoCounter;
+    bool countdownToYellowStarted = false;
 
     // Start is called before the first frame update
 
@@ -17,20 +26,22 @@ public class Highlight : MonoBehaviour
     {
         startTime = 0f;
         endTime = 0f;
+        countToYellow = 0f;
+        shiningStartTime = 0f;
+        shiningEndTime = 0f;
+
+        profitInfo = GameObject.Find("Przychód").GetComponent<Text>();
+        tobaccoCounter = GameObject.Find("TobaccoCounter").GetComponent<Text>();
+
         obj = GameObject.FindObjectsOfType(typeof(GameObject));
 
         List<GameObject> doUsuniecia = new List<GameObject>();
 
+        // int i = 0;
         foreach (GameObject gameObject in obj)
         {
-            Debug.Log(gameObject.name);
-        }
-
-       // int i = 0;
-        foreach (GameObject gameObject in obj)
-        {
-            if (gameObject.name == "Main Camera" || gameObject.name == "Plane" || gameObject.name == "Area Light")
-            {  
+            if (gameObject.name == "Main Camera" || gameObject.name == "Plane" || gameObject.name == "Area Light" || gameObject.name == "Canvas" || gameObject.name == "EventSystem" || gameObject.name == "Przychód" || gameObject.name == "TobaccoCounter")
+            {
                 doUsuniecia.Add(gameObject);
                 //Debug.Log("Będę usuwać " + i);
             }
@@ -39,83 +50,194 @@ public class Highlight : MonoBehaviour
 
         obj = obj.Except(doUsuniecia).ToArray();
 
+        originalColorsOfAllBuildings = new List<List<Color>>();
         foreach (GameObject gameObject in obj)
         {
-            Debug.Log(gameObject.name);
-        }
+            List<Color> objectMaterials = new List<Color>();
 
-        Debug.Log("Start");
-
-        originalColors = new List<List<Color>>();
-        foreach (GameObject gameObject in obj)
-        {
-
-                Debug.Log(gameObject.name);
-                List<Color> objectMaterials = new List<Color>();
-
-                foreach (var material in gameObject.GetComponent<Renderer>().materials)
-                {
-                    objectMaterials.Add(material.color);
-                    Debug.Log(material.color.ToString());
-                }
-                originalColors.Add(objectMaterials);
+            foreach (var material in gameObject.GetComponent<Renderer>().materials)
+            {
+                objectMaterials.Add(material.color);
+            }
+            originalColorsOfAllBuildings.Add(objectMaterials);
         }
     }
 
     // void is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0))
+        bool physicsRaycast = Physics.Raycast(ray, out hit);
+
+        if (Equals(clickedBuilding, "") && Input.GetMouseButtonDown(0) && physicsRaycast)
+        {
+            originalColorsOfSingleBuilding = storeOriginalColor(hit.transform.name);
             startTime = Time.time;
-        if (Input.GetMouseButtonUp(0))
+            clickedBuilding = hit.transform.name;
+            tobaccoCounter.transform.position = getClickedObjectPos(clickedBuilding);
+        }
+
+        if (Input.GetMouseButtonUp(0) && !countdownToYellowStarted)
             endTime = Time.time;
-        if (endTime - startTime > 0.5f)
+
+        if (endTime - startTime > 0.5f && !countdownToYellowStarted)
         {
 
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if ((Physics.Raycast(ray, out hit)))
+            if (physicsRaycast && !Equals(clickedBuilding, ""))
             {
-                int i = 0;
-                foreach (object o in obj)
-                {
-                    GameObject gameObject = (GameObject)o;
-                    if (gameObject.name != hit.transform.name)
-                    {
-                        Debug.Log("Koloruję na biało obiekt " + gameObject.name);
-                        int j = 0;
-                        foreach(var material in gameObject.GetComponent<Renderer>().materials)
-                        {
-                            Debug.Log(originalColors[i][j].ToString() + "i: "+i + "j: "+j);
-                            material.color = originalColors[i][j];
-                            j++;
-                        }
-                        Debug.Log("licba j: " + j);
-                    }
-                    i++;
-                }
-                Debug.Log("licba i: " + i);
 
-                if (hit.transform.name != "Plane")
-                {
-                    foreach(var hitmaterial in hit.transform.GetComponent<Renderer>().materials)
-                    {
-                        hitmaterial.color=new Color(hitmaterial.color[0],1, hitmaterial.color[2], hitmaterial.color[3]);
-                    }
-                }
+                setOrginalColorsForAllBuildings(clickedBuilding);
 
-                GameObject g = GameObject.Find("NameOfGameObject");
+                highlightClicked("green", clickedBuilding);
 
-                Debug.Log("Teraz kliknięty " + hit.transform.name);
-
-                clickedObject = hit.transform;
+                clickedBuilding = "";
 
             }
+        }
 
-            startTime = 0f;
-            endTime = 0f;
+        if (endTime - startTime < 0.5f && endTime - startTime > 0.0f)
+        {
+
+            if (clickedBuilding != "" && !Equals(checkIfColorChanged(clickedBuilding), "yellow"))
+            {
+                countToYellow = Time.time;
+                countdownToYellowStarted = true;
+            }
+
+            if (countToYellow - startTime > delayToPick && clickedBuilding != "")
+            {
+
+                shiningStartTime = Time.time;
+
+                highlightClicked("yellow", clickedBuilding);
+
+                countToYellow = 0f;
+
+                countdownToYellowStarted = false;
+
+            }
+        }
+
+        if (countToYellow - startTime <= 0)
+        {
+            tobaccoCounter.text = "";
+        }
+        else
+        {
+            tobaccoCounter.text = (Math.Truncate(countToYellow - startTime)).ToString() + "s";
+            Debug.Log("countToYellow "+ countToYellow);
+            Debug.Log("startTime "+ startTime);
+        }
+
+
+        if (clickedBuilding != "" && Equals(checkIfColorChanged(clickedBuilding), "yellow"))
+        {
+            shiningEndTime = Time.time;
+        }
+
+        if (shiningEndTime - shiningStartTime > 3.0f && clickedBuilding != "")
+        {
+            setOriginalColors(clickedBuilding, originalColorsOfSingleBuilding);
+            shiningStartTime = 0f;
+            shiningEndTime = 0f;
+            clickedBuilding = "";
+            startProfitforAll = startProfitforAll + profitIncrease;
+            profitInfo.text = "Przychód: " + startProfitforAll;
+        }
+    }
+
+    void setOriginalColors(string gameObjectName, List<Color> originalColorsOfSingleBuilding)
+    {
+        GameObject gameObject = GameObject.Find(gameObjectName);
+        int i = 0;
+        foreach (var material in gameObject.GetComponent<Renderer>().materials)
+        {
+            material.color = originalColorsOfSingleBuilding[i];
+            i++;
+        }
+    }
+
+    void setOrginalColorsForAllBuildings(String clickedBuilding)
+    {
+
+        int i = 0;
+
+        foreach (object o in obj)
+        {
+            GameObject gameObject2 = (GameObject)o;
+            if (gameObject2.name != clickedBuilding)
+            {
+
+                setOriginalColors(gameObject2.name, originalColorsOfAllBuildings[i]);
+                /*
+                int j = 0;
+                foreach (var material in gameObject2.GetComponent<Renderer>().materials)
+                {
+                    material.color = originalColorsOfAllBuildings[i][j];
+                    j++;
+                }
+                */
+            }
+            i++;
+        }
+    }
+
+    List<Color> storeOriginalColor(string gameObjectName)
+    {
+        GameObject gameObject = GameObject.Find(gameObjectName);
+
+        List<Color> originalColorsOfSingleBuilding = new List<Color>();
+
+        foreach (var material in gameObject.GetComponent<Renderer>().materials)
+        {
+            originalColorsOfSingleBuilding.Add(material.color);
+        }
+        return originalColorsOfSingleBuilding;
+    }
+
+    string checkIfColorChanged(string gameObjectName)
+    {
+        string returnColor;
+
+        GameObject gameObject = GameObject.Find(gameObjectName);
+
+        var material = gameObject.GetComponent<Renderer>().materials[0]; //wystarczy, że sprawdzimy pierwszy
+
+        if (material.color[0] == 1 && material.color[1] < 1)
+            returnColor = "red";
+        else if (material.color[0] == 1 && material.color[1] == 1)
+            returnColor = "yellow";
+        else if (material.color[0] < 1 && material.color[1] == 1)
+            returnColor = "green";
+        else returnColor = "black";
+
+        return returnColor;
+    }
+
+    Vector3 getClickedObjectPos(string clickedObjName)
+    {
+        GameObject gameObject = GameObject.Find(clickedObjName);
+        cam = GetComponent<Camera>();
+        return cam.WorldToScreenPoint(gameObject.transform.position);
+    }
+
+    void highlightClicked(String color, String clickedBuilding)
+    {
+        GameObject gameObject = GameObject.Find(clickedBuilding);
+
+        foreach (var material in gameObject.GetComponent<Renderer>().materials)
+        {
+            if (Equals(color, "green") && material.color[0] != 1 && material.color[1] != 1)
+            {
+                material.color = new Color(material.color[0], 1, material.color[2], material.color[3]);
+
+            }
+            else if (Equals(color, "yellow") && material.color[1] != 1)
+            {
+                material.color = new Color(1, 1, material.color[2], material.color[3]);
+            }
         }
     }
 }
---
+++ MobileGame/Assets/ReadyToPickup.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Threading;
+using System;
+
+public class ReadyToPickup : MonoBehaviour
+{
+    int delayToPick = 5;
+    double startTime, endTime, shiningStartTime, shiningEndTime;
+    string clickedBuilding = "";
+    object[] obj;
+    List<Color> originalColors = new List<Color>();
+    bool zazolcone = false;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        startTime = 0f;
+        endTime = 0f;
+        shiningStartTime = 0f;
+        shiningEndTime = 0f;
+    }
+
+    // void is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool physicsRaycast = Physics.Raycast(ray, out hit);
+
+        if (Equals(clickedBuilding, "") && Input.GetMouseButtonDown(0) && physicsRaycast)
+        {
+            originalColors = storeOriginalColor(hit.transform.name);
+            startTime = Time.time;
+            clickedBuilding = hit.transform.name;
+        }
+
+        if (clickedBuilding != "" && !Equals(checkIfColorChanged(clickedBuilding), "yellow"))
+            endTime = Time.time;
+
+        if (endTime - startTime > delayToPick && clickedBuilding != "")
+        {
+
+            //Koloruje zaznaczony budynek
+            GameObject g = GameObject.Find(clickedBuilding);
+
+            shiningStartTime = Time.time;
+
+            foreach (var material in g.GetComponent<Renderer>().materials)
+            {
+                if (material.color[1] != 1) //jeśli nie zielone :P
+                    material.color = new Color(1, 1, material.color[2], material.color[3]);
+            }
+            startTime = 0f;
+            endTime = 0f;
+        }
+
+        if (clickedBuilding != "" && Equals(checkIfColorChanged(clickedBuilding), "yellow"))
+            shiningEndTime = Time.time;
+
+        if (shiningEndTime - shiningStartTime > 3.0f && clickedBuilding != "")
+        {
+
+            setOriginalColors(clickedBuilding, originalColors);
+
+            shiningStartTime = 0f;
+            shiningEndTime = 0f;
+
+            clickedBuilding = "";
+        }
+    }
+
+    void setOriginalColors(string gameObjectName, List<Color> originalColors)
+    {
+        GameObject gameObject = GameObject.Find(gameObjectName);
+        int i = 0;
+        foreach (var material in gameObject.GetComponent<Renderer>().materials)
+        {
+            material.color = originalColors[i];
+            i++;
+        }
+    }
+
+    List<Color> storeOriginalColor(string gameObjectName)
+    {
+        GameObject gameObject = GameObject.Find(gameObjectName);
+
+        List<Color> originalColors = new List<Color>();
+
+        foreach (var material in gameObject.GetComponent<Renderer>().materials)
+        {
+            originalColors.Add(material.color);
+        }
+        return originalColors;
+    }
+
+    string checkIfColorChanged(string gameObjectName)
+    {
+        string returnColor;
+
+        GameObject gameObject = GameObject.Find(gameObjectName);
+
+        var material = gameObject.GetComponent<Renderer>().materials[0]; //wystarczy, że sprawdzimy pierwszy
+
+        if (material.color[0] == 1 && material.color[1] < 1)
+            returnColor = "red";
+        else if (material.color[0] == 1 && material.color[1] == 1)
+            returnColor = "yellow";
+        else if (material.color[0] < 1 && material.color[1] == 1)
+            returnColor = "green";
+        else returnColor = "black";
+
+        return returnColor;
+    }
+}
