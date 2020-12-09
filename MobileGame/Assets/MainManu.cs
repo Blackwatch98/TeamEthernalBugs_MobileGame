d0e14039b63ae3050fcf5ce70907acfeb5746064
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-12-09 01:26:48 +0100

    Added gold PlayerPrefs and added Daily Reward menu entry

--- MobileGame/Assets/MainManu.cs
+++ MobileGame/Assets/MainManu.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 public class MainManu : MonoBehaviour
 {
-public void PlayGame()
-    {
-       SceneManager.LoadScene("TestBuildings");
-    }
+   public void PlayGame()
+   {
+      SceneManager.LoadScene("TestBuildings");
+   }
+
+   public void DailyReward()
+   {
+      int gold = PlayerPrefs.GetInt("gold", 0);
+      PlayerPrefs.SetInt("gold", gold + 100);
+      EditorUtility.DisplayDialog("Daily reward", "Gained 100 gold", "OK");
+   }
 }
