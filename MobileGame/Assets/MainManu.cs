46fcde00f5db4e643c4bf6255b7ea10594898563
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-12-09 09:44:15 +0100

    Added measuring time and daily reward is available for 20 seconds

--- MobileGame/Assets/MainManu.cs
+++ MobileGame/Assets/MainManu.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using System;
 public class MainManu : MonoBehaviour
 {
+   public static readonly double REWARD_DELAY = 20.0;
    public void PlayGame()
    {
       SceneManager.LoadScene("TestBuildings");
@@ -12,8 +14,37 @@ public class MainManu : MonoBehaviour
 
    public void DailyReward()
    {
-      int gold = PlayerPrefs.GetInt("gold", 0);
-      PlayerPrefs.SetInt("gold", gold + 100);
-      EditorUtility.DisplayDialog("Daily reward", "Gained 100 gold", "OK");
+      if (CheckForDailyRewardAvailable())
+      {
+         int gold = PlayerPrefs.GetInt("gold", 0);
+         int day = PlayerPrefs.GetInt("day", 0) + 1;
+         int gainedGold = 100 * day;
+         PlayerPrefs.SetInt("gold", gold + gainedGold);
+         PlayerPrefs.SetInt("day", day);
+         EditorUtility.DisplayDialog("Daily reward", "Day: " + day.ToString() + ", gained " + gainedGold.ToString() + " gold", "OK");
+      }
+   }
+
+   public bool CheckForDailyRewardAvailable()
+   {
+      string rewardDatetime = PlayerPrefs.GetString("rewardDatetime");
+      if (string.IsNullOrEmpty(rewardDatetime))
+      {
+         PlayerPrefs.SetString("rewardDatetime", DateTime.Now.ToString());
+         return true;
+      }
+      else
+      {
+         DateTime currentDatetime = DateTime.Now;
+         DateTime previousDatetime = DateTime.Parse(rewardDatetime);
+
+         double elapsedSeconds = (currentDatetime - previousDatetime).TotalSeconds;
+         if (elapsedSeconds >= REWARD_DELAY)
+         {
+            PlayerPrefs.SetString("rewardDatetime", currentDatetime.ToString());
+            return true;
+         }
+         return false;
+      }
    }
 }
