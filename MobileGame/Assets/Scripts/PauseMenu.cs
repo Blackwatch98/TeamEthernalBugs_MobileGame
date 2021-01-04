d7fc65dd510458b9ce5d7a3daf9dcd8468a61077
Blackwatch98 <danielduczyminski@gmail.com>
2021-01-04 21:42:46 +0100

    player class just in case, player income save to playerprefs

---
+++ MobileGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button resetButton = gameObject.GetComponent<Button>();
+        resetButton.onClick.AddListener(() => resetPrefs());
+    }
+
+    private void resetPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Zresetowałem prefsy");
+    }
+}
