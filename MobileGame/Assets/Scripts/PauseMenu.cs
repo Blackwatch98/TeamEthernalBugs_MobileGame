eb417efdd24457139fc1143c76a1ef4ed4b72519
Blackwatch98 <danielduczyminski@gmail.com>
2021-01-12 17:14:29 +0100

    test app icon, pause menu, keystore created

--- MobileGame/Assets/Scripts/PauseMenu.cs
+++ MobileGame/Assets/Scripts/PauseMenu.cs
@@ -7,15 +7,32 @@ using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        Button resetButton = gameObject.GetComponent<Button>();
-        resetButton.onClick.AddListener(() => resetPrefs());
-    }
+    public static bool isGamePaused = false;
+
 
     private void resetPrefs()
     {
-        PlayerPrefs.DeleteAll();
-        Debug.Log("Zresetowałem prefsy");
+        //PlayerPrefs.DeleteAll();
+       // Debug.Log("Zresetowałem prefsy");
+        Pause();
+    }
+
+    public void Pause()
+    {
+        gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void Resume()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
