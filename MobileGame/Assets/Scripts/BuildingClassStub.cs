ffd8305850a933a7394861146930e86d2c738e21
Patryk.Stepien <215886@edu.p.lodz.pl>
2021-01-22 20:18:00 +0100

    Added BuildingClassStub

---
+++ MobileGame/Assets/Scripts/BuildingClassStub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class BuildingClassStub
+{
+   public GameObject specifiedBuilding;
+   public bool isOwned = false;
+   public string type = null;
+   public float price;
+   public float income;
+   public float workerIncomeIncrease;
+   public int maxNumOfWorkers;
+   public OwnedPanelScript ownedPanelScript = null;
+   public NotOwnedPanelScript notOwnedPanelScript = null;
+   public string buildingName;
+   public Sprite buildingImage;
+   public float startWorkerCost;
+}
